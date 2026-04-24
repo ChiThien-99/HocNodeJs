90487a2cf0194d1687548aa5583b95fdc2ece93c
ChiThien-99 <thienphuc19992003@gmail.com>
2026-04-24 21:36:05 +0700

    updatecode 24042026

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,20 +1,30 @@
-
-const todolist=[
-    {id:1,task:"Ăn sáng",complete:false},
-    {id:2,task:"Đi làm",complete:false},
-]
-export const getTodo=(req,res)=>{
-    res.render("todo.ejs",{todolist});
-}
-export const postTodo=(req,res)=>{
-    try {
-        let newTodo=req.body;
-        console.log(req.body);
-        newTodo.id=todolist.length>0?Math.max(...todolist.map(t=>t.id))+1:1;
-        todolist.push(newTodo);
-        console.log(todolist);
-        res.status(201).json(newTodo);
-    } catch (error) {
-        res.json(error);
-    }
-}
+let todolist = [
+  { id: 1, task: "Ăn sáng", complete: false },
+  { id: 2, task: "Đi làm", complete: false },
+];
+export const getTodo = (req, res) => {
+  res.render("todo.ejs", { todolist });
+};
+export const postTodo = (req, res) => {
+  try {
+    let newTodo = req.body;
+    console.log(req.body);
+    newTodo.id =
+      todolist.length > 0 ? Math.max(...todolist.map((t) => t.id)) + 1 : 1;
+    todolist.push(newTodo);
+    console.log(todolist);
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.json(error);
+  }
+};
+export const deleteTodo = (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    todolist = todolist.filter((item) => item.id != id);
+    res.json({ mess: "Xóa thành công", status: 200 });
+  } catch (error) {
+    res.json({ mess: "Xóa không thành công", error: error.message });
+  }
+};
--- routers/todo.router.js
+++ routers/todo.router.js
@@ -1,7 +1,8 @@
-const express=require("express");
-const router=express.Router();
-const todoController=require("../controllers/todo.controller");
-const prefix="/todo";
-router.get(prefix,todoController.getTodo);
-router.post(`${prefix}/create`,todoController.postTodo);
-module.exports=router;
+const express = require("express");
+const router = express.Router();
+const todoController = require("../controllers/todo.controller");
+const prefix = "/todo";
+router.get(prefix, todoController.getTodo);
+router.post(`${prefix}/create`, todoController.postTodo);
+router.delete(`${prefix}/delete/:id`, todoController.deleteTodo);
+module.exports = router;
