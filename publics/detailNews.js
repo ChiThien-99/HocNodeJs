97e46b62d0926718bca6008b5d66f04876eec615
ChiThien-99 <thienphuc19992003@gmail.com>
2026-05-21 17:41:56 +0700

    updatecode2105

--- publics/detailNews.js
+++ publics/detailNews.js
@@ -146,3 +146,26 @@ document.getElementById("btnShareZL").addEventListener("click", () => {
     "width=600,height=500,resizable=yes,scrollbars=yes",
   );
 });
+document.addEventListener("DOMContentLoaded",()=>{
+  const hamburgerBtn=document.getElementById("hamburgerBtn");
+  const navMenu=document.getElementById("navMenu");
+  if (hamburgerBtn&&navMenu) {
+    hamburgerBtn.addEventListener("click",()=>{
+      hamburgerBtn.classList.toggle("active");
+      navMenu.classList.toggle("active");
+      if (navMenu.classList.contains("active")) {
+        document.body.classList.add("no-scroll");
+      }else{
+        document.body.classList.remove("no-scroll");
+      }
+    })
+    const navLink=navMenu.querySelectorAll("a");
+    navLink.forEach(link=>{
+      link.addEventListener("click",()=>{
+        hamburgerBtn.classList.remove("active");
+        navMenu.classList.remove("active");
+        document.body.classList.remove("no-scroll");
+      })
+    })
+  }
+})
